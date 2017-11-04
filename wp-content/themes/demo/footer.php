4bf77cf0c01688f645ec336ff9539917d1c8de85
Dai Le <ledaicntt@gmail.com>
2017-11-04 15:09:01 +0700

    initital index

---
+++ wp-content/themes/demo/footer.php
@@ -0,0 +1,15 @@
+<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.easing.1.3.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.fancybox.pack.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.fancybox-media.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/google-code-prettify/prettify.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/portfolio/jquery.quicksand.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/portfolio/setting.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/jquery.flexslider.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/animate.js"></script>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ wp-content/themes/demo/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Moderna - Bootstrap 3 flat corporate template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <!-- css -->
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/fancybox/jquery.fancybox.css" rel="stylesheet">
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/jcarousel.css" rel="stylesheet" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/flexslider.css" rel="stylesheet" />
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/css/style.css" rel="stylesheet" />
+
+    <!-- Theme skin -->
+    <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/skins/default.css" rel="stylesheet" />
+
+    <!-- =======================================================
+    Theme Name: Moderna
+    Theme URL: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+    ======================================================= -->
+
+</head>
+
+<body>
